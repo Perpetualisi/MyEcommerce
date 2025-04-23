499f59d3b3dd16d20429c36b788644bfa5dfa8e5
Isi Name <perpetualokan5@gmail.com>
2025-04-23 17:25:47 +0100

    Fix: corrected CSS file import case in SpecialOffers.jsx

--- src/Components/SpecialOffers/SpecialOffers.jsx
+++ src/Components/SpecialOffers/SpecialOffers.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';  
-import './specialOffers.css';
+import './SpecialOffers.css';
 
 const SpecialOffers = () => {
   const offers = [
